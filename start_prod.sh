bdf1f668a91bf414ca51dfd2cb10f2fd591eca45
Maksim Panchuk <panchescosenior@Maksims-MacBook-Pro.local>
2025-03-12 15:15:04 +0300

    final

--- start_prod.sh
+++ start_prod.sh
@@ -8,4 +8,4 @@ mkdir -p voice
 
 # Запускаем сервер с Gunicorn
 # Используем 4 рабочих процесса и таймаут 300 секунд (5 минут) для длительных операций генерации
-gunicorn --workers=4 --timeout=500 --bind=0.0.0.0:8080 wsgi:app 
+gunicorn --workers=1 --timeout=500 --bind=0.0.0.0:8080 wsgi:app 
